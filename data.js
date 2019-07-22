70461354af0a477c6b2e85b8ae7afb58792114c6
chinedu onyeoka <chinedu.onyeoka@gmail.com>
2019-07-22 13:11:43 +0100

    created a backend server to serve our clients

--- data.js
+++ data.js
@@ -7,40 +7,40 @@ module.exports = function () {
                 description: "A boat for One person", price: 274
             },
             {
-                id: 1, name: "kbala", category: "Watersports",
-                description: "A boat for One person", price: 274
+                id: 2, name: "Lifejacket", category: "Watersports",
+                description: "Protective and fashionable", price: 984
             },
             {
-                id: 1, name: "kbala", category: "Watersports",
-                description: "A boat for One person", price: 274
+                id: 3, name: "sushaba", category: "Chess",
+                description: "play smartly", price: 200
             },
             {
-                id: 1, name: "kbala", category: "Watersports",
-                description: "A boat for One person", price: 274
+                id: 4, name: "CornerFlag", category: "Soccer",
+                description: "Fifa approved ", price: 2004
             },
             {
-                id: 1, name: "kbala", category: "Watersports",
-                description: "A boat for One person", price: 274
+                id: 5, name: "chuka", category: "Chess",
+                description: "Dont get caught", price: 222
             },
              {
-                id: 1, name: "kbala", category: "Watersports",
-                description: "A boat for One person", price: 274
+                id: 6, name: "Adrea", category: "Watersports",
+                description: "A boat for fishing pond", price: 574
             },
             {
-                id: 1, name: "kbala", category: "Watersports",
-                description: "A boat for One person", price: 274
+                id: 7, name: "Mathew", category: "Soccer",
+                description: "can play both wings", price: 543
             },
             {
-                id: 1, name: "kbala", category: "Watersports",
-                description: "A boat for One person", price: 274
+                id: 8, name: "saerv", category: "Chess",
+                description: "Playing and hacking away", price: 884
             },
             {
-                id: 1, name: "kbala", category: "Watersports",
-                description: "A boat for One person", price: 274
+                id: 9, name: "dango", category: "Watersports",
+                description: "A shipwreck for One person", price: 994
             },
             {
-                id: 1, name: "kbala", category: "Watersports",
-                description: "A boat for One person", price: 274
+                id: 10, name: "chukwu", category: "Soccer",
+                description: "Affordable for all", price: 4
             }
         ],
         orders: []
