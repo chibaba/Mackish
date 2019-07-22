f20e1db61aa9b1ca46574efdce6d5e2b562a5123
chinedu onyeoka <chinedu.onyeoka@gmail.com>
2019-07-22 14:07:10 +0100

    created a static data for the client side

---
+++ src/data/placeholderData.js
@@ -0,0 +1,29 @@
+export const data = {
+    categories: ["Watersport", "Soccer", "Chess", "Running"],
+    products: [
+        {
+            id: 1, name: "p1", category: "Watersports",
+            description: "P1 (Watersports)", price: 3
+        },
+        {
+            id: 2, name: "p2", category: "Running",
+            description: "P2 (Running)", price: 3
+        },
+        {
+            id: 3, name: "p3", category: "Running",
+            description: "P3 (Running)", price: 3
+        },
+        {
+            id: 4, name: "p4", category: "Chess",
+            description: "P4 (Chess)", price: 8
+        },
+        {
+            id: 5, name: "p5", category: "Running",
+            description: "P5 (Running)", price: 7
+        },
+        {
+            id: 6, name: "p6", category: "Soccer",
+            description: "P6 (Soccer)", price: 6
+        }
+    ]
+}
