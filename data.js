a21f86207a05702fad8b470f74f4d1f6121e0fc7
chinedu onyeoka <chinedu.onyeoka@gmail.com>
2019-07-22 11:28:54 +0100

    created a data file to show the product descripyion and product list and an empty order that the user hasnt requested for

---
+++ data.js
@@ -0,0 +1,48 @@
+module.exports = function () {
+    return {
+        categories: ["Watersports", "Soccer", "Chess"],
+        products: [
+            {
+                id: 1, name: "kbala", category: "Watersports",
+                description: "A boat for One person", price: 274
+            },
+            {
+                id: 1, name: "kbala", category: "Watersports",
+                description: "A boat for One person", price: 274
+            },
+            {
+                id: 1, name: "kbala", category: "Watersports",
+                description: "A boat for One person", price: 274
+            },
+            {
+                id: 1, name: "kbala", category: "Watersports",
+                description: "A boat for One person", price: 274
+            },
+            {
+                id: 1, name: "kbala", category: "Watersports",
+                description: "A boat for One person", price: 274
+            },
+             {
+                id: 1, name: "kbala", category: "Watersports",
+                description: "A boat for One person", price: 274
+            },
+            {
+                id: 1, name: "kbala", category: "Watersports",
+                description: "A boat for One person", price: 274
+            },
+            {
+                id: 1, name: "kbala", category: "Watersports",
+                description: "A boat for One person", price: 274
+            },
+            {
+                id: 1, name: "kbala", category: "Watersports",
+                description: "A boat for One person", price: 274
+            },
+            {
+                id: 1, name: "kbala", category: "Watersports",
+                description: "A boat for One person", price: 274
+            }
+        ],
+        orders: []
+    }
+}
