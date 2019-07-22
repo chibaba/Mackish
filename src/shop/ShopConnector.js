93ac08f4a4238025e98a60e54b95ad80522cd9bd
chinedu onyeoka <chinedu.onyeoka@gmail.com>
2019-07-23 00:29:53 +0100

    the shopconnector file created

--- src/shop/ShopConnector.js
+++ src/shop/ShopConnector.js
@@ -4,3 +4,35 @@ import { connect } from "react-redux";
 import { loadData }  from "../data/ActionCreators";
 import { DataTypes } from "../data/Types";
 import { Shop } from "./Shop";
+
+const mapStateToProps = (dataStore) => ({
+    ...dataStore
+})
+
+const mapDispatchToProps = {
+    loadData
+}
+
+const filterProducts = (products = [], category) =>
+(!category || category === "All")
+? products
+: products.filter(p => p.category.toLowerCase() === category.toLowerCase());
+
+export const ShopConnector = connect(mapStateToProps, mapDispatchToProps)(
+    class extends Component {
+        render() {
+            return <Switch>
+                <Route path="/Shop/products/:category ?"
+                render={ (routeProps) =>
+                <Shop {...this.props} {...routeProps} 
+               products={filterProducts(this.props.products,
+                routeProps.match.params.category)} />} />
+                <Redirect to="/shop/products" />
+            </Switch>
+        }
+        componentDidMount() {
+            this.props.loadData(DataTypes.CATEGORIES);
+            this.props.loadData(DataTypes.PRODUCTS);
+        }
+    }
+)
