27b94e3708e2969ac619c03d142300d8606c99c8
chinedu onyeoka <chinedu.onyeoka@gmail.com>
2019-07-29 06:14:21 +0100

    edited reducer

--- src/data/CartReducer.js
+++ src/data/CartReducer.js
@@ -17,6 +17,30 @@ export const CartReducer = (storeReducer, action) => {
             newStore.cartPrice += p.price * q;
             return newStore;
 
-            
+          case ActionTypes.CART_UPDATE:
+              newStore.cart = newStore.cart.map(item => {
+                  if(item.product.id === action.payload.product.id) {
+                    const diff = action.payload.quantity - item.quantity
+                    newStore.cartItems += diff;
+                    newStore.cartPrice += (item.product.price * diff);
+                    return action.payload;
+              } else {
+                  return item;
+              }
+              });
+              return newStore;
+              
+         case ActionTypes.CART_REMOVE:
+             let selection = newStore.cart.find(item => 
+                item.product.id === actio.payload.id);
+                newStore.cartItem -= selection.quantity;
+                newStore.cartPrice -= selection.quantity * selection.product.price;
+                newStore.cart = newStore.cart.filter(item => item != selection )
+                return newStore;
+
+        case ActionTypes.CART_CLEAR:
+            return {...storeData, cart: [], cartItems: 0, cartPrice: 0}
+            default:
+             return storeData || {}
     }
 }
